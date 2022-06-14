d74abb5235f5029301a4be6f5e60580afd8256a9
linuxis159 <dlrrud159@naver.com>
2022-06-14 21:42:13 +0900

    2-1

---
+++ src/chapter_02_variable/VariableInit.java
@@ -0,0 +1,12 @@
+package chapter_02_variable;
+
+public class VariableInit {
+    public static void main(String args[]){
+        int value = 0;
+        int chain = null;
+        int mobyus;
+        System.out.println(value);
+        System.out.println(chain);
+        System.out.println(mobyus);
+    }
+}
